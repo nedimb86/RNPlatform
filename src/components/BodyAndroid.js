2e5d232f1f0e9ef3cbbc0a89fc08c24ef94343a2
Nedim Becirovic <nedimb86@gmail.com>
2018-05-12 23:10:35 +0200

    Add Body to show Version

--- src/components/BodyAndroid.js
+++ src/components/BodyAndroid.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { View, Text, Platform } from 'react-native';
 import {styles} from "../theme/Style";
 
 export const BodyAndroid = () => (
   <View style={styles.body}>
     <Text style={styles.h1}>This is Android App!</Text>
+    <Text style={styles.h1}>You are running Android {Platform.Version}</Text>
   </View>
 );
--- src/components/BodyIOS.js
+++ src/components/BodyIOS.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { View, Text, Platform } from 'react-native';
 import {styles} from "../theme/Style";
 
 export const BodyIOS = () => (
   <View style={styles.body}>
     <Text style={styles.h1}>This is iOS App!</Text>
+    <Text style={styles.h1}>You are running iOS {parseInt(Platform.Version, 10)}</Text>
   </View>
 );
